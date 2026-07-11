cb38a157f31dc2179ba7118654cb7acbfa2cc720
LeoAugustoDev <leoaugustoalves58@gmail.com>
2026-07-11 15:51:01 -0300

    first commit

---
+++ src/test/java/br/com/leo/api_gestao_funcionarios/ApiGestaoFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leo.api_gestao_funcionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestaoFuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
